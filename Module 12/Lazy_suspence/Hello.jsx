9686e941df695b44e5091eeae34d0f4044bd7ed5
Mukesh189 <109957091+Mukesh189@users.noreply.github.com>
2023-11-23 12:27:33 +0530

    Add files via upload

---
+++ Module 12/Lazy_suspence/Hello.jsx
@@ -0,0 +1,15 @@
+import React,{useEffect}from 'react'
+
+function Hello() {
+   
+    useEffect(()=>{
+    },[])
+   
+    return (
+        <div>
+            <h1>Hello..</h1>
+        </div>
+  )
+}
+
+export default Hello
